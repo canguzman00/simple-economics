4e5d3f24eee4c45a734ca29013e5bc98496b51ec
Carlos Guzman <canguzman@gmail.com>
2026-05-09 12:25:08 -0400

    fix: remove email provider, fix serverExternalPackages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["nodemailer"],
+    serverExternalPackages: ["nodemailer"],
   },
   images: {
     remotePatterns: [
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,6 @@
 import { NextAuthOptions, getServerSession } from "next-auth";
 import { useSession as useNextAuthSession } from "next-auth/react";
 import GoogleProvider from "next-auth/providers/google";
-import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 
@@ -11,17 +10,6 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST!,
-        port: Number(process.env.EMAIL_SERVER_PORT!),
-        auth: {
-          user: process.env.EMAIL_SERVER_USER!,
-          pass: process.env.EMAIL_SERVER_PASSWORD!,
-        },
-      },
-      from: process.env.EMAIL_FROM!,
     }),
   ],
   session: {
