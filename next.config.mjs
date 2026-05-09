4cde4dd7b20a0b538e4049ded15b5209205dffd8
Carlos Guzman <canguzman@gmail.com>
2026-05-09 12:20:06 -0400

    fix: resolve fs module error with nodemailer

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["nodemailer"],
+  },
   images: {
     remotePatterns: [
       {
