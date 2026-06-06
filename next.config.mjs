7ba31782b904d48240a4bbde8cb9c1f4fdfda27c
canguzman00 <canguzman@gmail.com>
2026-06-06 00:52:23 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,24 +3,19 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["nodemailer", "prisma", "@prisma/client", "yahoo-finance2"],
   },
+  webpack: function(config, { isServer }) {
+    if (isServer) {
+      config.externals = config.externals || []
+      config.externals.push('yahoo-finance2')
+    }
+    return config
+  },
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "img.youtube.com",
-      },
-      {
-        protocol: "https",
-        hostname: "i.ytimg.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "img.youtube.com" },
+      { protocol: "https", hostname: "i.ytimg.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
 };
