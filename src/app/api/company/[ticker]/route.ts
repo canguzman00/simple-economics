8227baa443707c1050eb6cf560e6655b763e0d90
canguzman00 <canguzman@gmail.com>
2026-06-06 01:32:59 -0400

    Update route.ts

--- src/app/api/company/[ticker]/route.ts
+++ src/app/api/company/[ticker]/route.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-require-imports */
 import { NextRequest, NextResponse } from 'next/server'
 
 export const runtime = 'nodejs'
